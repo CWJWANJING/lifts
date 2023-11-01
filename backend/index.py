f5b830bb9045c4daeafdb984572d1a49601e739f
CWJWANJING <wanjingchencwj@gmail.com>
2023-11-01 16:19:37 +0000

    Root endpoint can return template date

--- backend/index.py
+++ backend/index.py
@@ -1,7 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+mock_props = [
+    {
+        "floors": [2, 1, "G"],
+        "currFloor": "G",
+        "direction": "up"
+     }
+]
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def get_liftInfo():
+    return mock_props, 200
