d01719a749aa15a4198070224cd9ab5e2a8c81a1
CWJWANJING <wanjingchencwj@gmail.com>
2023-11-01 20:30:52 +0000

    Added test for receive POST data

--- backend/tests/test_root.py
+++ backend/tests/test_root.py
@@ -1,4 +1,5 @@
 from __init__ import client
+import json
 
 mock_props = [{
     "currFloor": "G", 
@@ -9,5 +10,11 @@ mock_props = [{
 def test_get(client):
     response = client.get("/")
     expected_data = str(mock_props).replace(" ", "").replace("\'", "\"")+"\n"
+    assert response.data.decode("utf-8") == expected_data
+    assert response.status_code == 200
+
+def test_post(client):
+    response = client.post("/", data=json.dumps(["1"]))
+    expected_data = "Data received"
     assert response.data.decode("utf-8") == expected_data
     assert response.status_code == 200
