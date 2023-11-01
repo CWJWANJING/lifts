2c65b09d28631b73e81ea70afda041a567aad7d8
CWJWANJING <wanjingchencwj@gmail.com>
2023-11-01 16:19:17 +0000

    test root endpoint to return template data

---
+++ backend/test_root.py
@@ -0,0 +1,14 @@
+import json
+from __init__ import client
+
+mock_props = [{
+    "currFloor": "G", 
+    "direction": "up", 
+    "floors": [2,1,"G"]
+    }]
+
+def test_get(client):
+    response = client.get("/")
+    expected_data = str(mock_props).replace(" ", "").replace("\'", "\"")+"\n"
+    assert response.data.decode("utf-8") == expected_data
+    assert response.status_code == 200
