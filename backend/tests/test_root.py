039bdf941908e456ba245093e51a71a49b8275c4
CWJWANJING <wanjingchencwj@gmail.com>
2023-11-03 18:07:06 +0000

    Updated BE test

--- backend/tests/test_root.py
+++ backend/tests/test_root.py
@@ -4,8 +4,10 @@ import json
 mock_props = [{
     "currFloor": "G", 
     "direction": "up", 
-    "floors": [2,1,"G"]
+    "floors": ["2","1","G"]
     }]
+
+pressed_floors = [[]]
 
 def test_get(client):
     response = client.get("/")
@@ -13,8 +15,12 @@ def test_get(client):
     assert response.data.decode("utf-8") == expected_data
     assert response.status_code == 200
 
+mock_post = {
+    "lift": 0, 
+    "pressed": "1"
+}
 def test_post(client):
-    response = client.post("/", data=json.dumps(["1"]))
+    response = client.post("/", data=json.dumps(mock_post))
     expected_data = "Data received"
     assert response.data.decode("utf-8") == expected_data
     assert response.status_code == 200
